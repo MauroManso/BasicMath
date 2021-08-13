d9bb8074687a33eac3b8b7926779eb6680fe2aaf
Mauro Lucas da Silva Manso <maurosmanso@gmail.com>
2021-08-13 09:39:34 -0300

    Prime lists design fixes

--- BasicMathBase/FormPrimeSieve.Designer.cs
+++ BasicMathBase/FormPrimeSieve.Designer.cs
@@ -31,16 +31,18 @@ namespace BasicMathBase
         {
             this.panel3 = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.lblTimeElapsed = new System.Windows.Forms.Label();
+            this.lblNumberOfPrimes = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
             this.txtboxUpToPrime = new System.Windows.Forms.TextBox();
             this.btnListPrime = new System.Windows.Forms.Button();
             this.lbl_Lista = new System.Windows.Forms.Label();
             this.richTextBoxLista = new System.Windows.Forms.RichTextBox();
-            this.btnClear = new System.Windows.Forms.Button();
-            this.btnPrint = new System.Windows.Forms.Button();
-            this.lblNumberOfPrimes = new System.Windows.Forms.Label();
-            this.lblTimeElapsed = new System.Windows.Forms.Label();
-            this.lblPercentage = new System.Windows.Forms.Label();
             this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel3
@@ -65,12 +67,90 @@ namespace BasicMathBase
             this.lblTitle.TabIndex = 2;
             this.lblTitle.Text = "Números Primos por Crivo";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblPercentage);
+            this.panel1.Controls.Add(this.lblTimeElapsed);
+            this.panel1.Controls.Add(this.lblNumberOfPrimes);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnPrint);
+            this.panel1.Controls.Add(this.txtboxUpToPrime);
+            this.panel1.Controls.Add(this.btnListPrime);
+            this.panel1.Controls.Add(this.lbl_Lista);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1128, 217);
+            this.panel1.TabIndex = 24;
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPercentage.Location = new System.Drawing.Point(530, 153);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(42, 25);
+            this.lblPercentage.TabIndex = 4143;
+            this.lblPercentage.Text = "0 %";
+            // 
+            // lblTimeElapsed
+            // 
+            this.lblTimeElapsed.AutoSize = true;
+            this.lblTimeElapsed.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTimeElapsed.Location = new System.Drawing.Point(609, 117);
+            this.lblTimeElapsed.Name = "lblTimeElapsed";
+            this.lblTimeElapsed.Size = new System.Drawing.Size(181, 25);
+            this.lblTimeElapsed.TabIndex = 4144;
+            this.lblTimeElapsed.Text = "Tempo passado: 0Ms";
+            // 
+            // lblNumberOfPrimes
+            // 
+            this.lblNumberOfPrimes.AutoSize = true;
+            this.lblNumberOfPrimes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNumberOfPrimes.Location = new System.Drawing.Point(325, 117);
+            this.lblNumberOfPrimes.Name = "lblNumberOfPrimes";
+            this.lblNumberOfPrimes.Size = new System.Drawing.Size(187, 25);
+            this.lblNumberOfPrimes.TabIndex = 4145;
+            this.lblNumberOfPrimes.Text = "Há 0 Números Primos";
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatAppearance.BorderSize = 2;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnClear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClear.Location = new System.Drawing.Point(872, 105);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(135, 55);
+            this.btnClear.TabIndex = 4141;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.FlatAppearance.BorderSize = 2;
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPrint.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnPrint.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPrint.Location = new System.Drawing.Point(115, 105);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(135, 55);
+            this.btnPrint.TabIndex = 4142;
+            this.btnPrint.Text = "Imprimir";
+            this.btnPrint.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
             // txtboxUpToPrime
             // 
-            this.txtboxUpToPrime.Location = new System.Drawing.Point(494, 147);
+            this.txtboxUpToPrime.Location = new System.Drawing.Point(506, 67);
             this.txtboxUpToPrime.Name = "txtboxUpToPrime";
             this.txtboxUpToPrime.Size = new System.Drawing.Size(101, 31);
-            this.txtboxUpToPrime.TabIndex = 4130;
+            this.txtboxUpToPrime.TabIndex = 4138;
             // 
             // btnListPrime
             // 
@@ -79,10 +159,10 @@ namespace BasicMathBase
             this.btnListPrime.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnListPrime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.btnListPrime.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnListPrime.Location = new System.Drawing.Point(625, 138);
+            this.btnListPrime.Location = new System.Drawing.Point(637, 58);
             this.btnListPrime.Name = "btnListPrime";
             this.btnListPrime.Size = new System.Drawing.Size(135, 55);
-            this.btnListPrime.TabIndex = 4133;
+            this.btnListPrime.TabIndex = 4140;
             this.btnListPrime.Text = "Listar";
             this.btnListPrime.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnListPrime.UseVisualStyleBackColor = true;
@@ -92,86 +172,23 @@ namespace BasicMathBase
             // 
             this.lbl_Lista.AutoSize = true;
             this.lbl_Lista.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbl_Lista.Location = new System.Drawing.Point(369, 150);
+            this.lbl_Lista.Location = new System.Drawing.Point(381, 70);
             this.lbl_Lista.Name = "lbl_Lista";
             this.lbl_Lista.Size = new System.Drawing.Size(100, 25);
-            this.lbl_Lista.TabIndex = 4131;
+            this.lbl_Lista.TabIndex = 4139;
             this.lbl_Lista.Text = "Primos até:";
             // 
             // richTextBoxLista
             // 
-            this.richTextBoxLista.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(67)))), ((int)(((byte)(97)))));
-            this.richTextBoxLista.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.richTextBoxLista.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.richTextBoxLista.Location = new System.Drawing.Point(0, 301);
+            this.richTextBoxLista.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxLista.Location = new System.Drawing.Point(29, 312);
+            this.richTextBoxLista.Margin = new System.Windows.Forms.Padding(20);
             this.richTextBoxLista.Name = "richTextBoxLista";
-            this.richTextBoxLista.ReadOnly = true;
-            this.richTextBoxLista.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBoxLista.Size = new System.Drawing.Size(1128, 593);
-            this.richTextBoxLista.TabIndex = 4134;
+            this.richTextBoxLista.Size = new System.Drawing.Size(1070, 553);
+            this.richTextBoxLista.TabIndex = 25;
             this.richTextBoxLista.Text = "";
-            // 
-            // btnClear
-            // 
-            this.btnClear.FlatAppearance.BorderSize = 2;
-            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClear.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnClear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClear.Location = new System.Drawing.Point(860, 185);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(135, 55);
-            this.btnClear.TabIndex = 4135;
-            this.btnClear.Text = "Limpar";
-            this.btnClear.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnClear.UseVisualStyleBackColor = true;
-            this.btnClear.Click += new System.EventHandler(this.btnClean_Click);
-            // 
-            // btnPrint
-            // 
-            this.btnPrint.FlatAppearance.BorderSize = 2;
-            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPrint.Font = new System.Drawing.Font("Nirmala UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnPrint.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnPrint.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPrint.Location = new System.Drawing.Point(103, 185);
-            this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(135, 55);
-            this.btnPrint.TabIndex = 4136;
-            this.btnPrint.Text = "Imprimir";
-            this.btnPrint.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnPrint.UseVisualStyleBackColor = true;
-            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
-            // 
-            // lblNumberOfPrimes
-            // 
-            this.lblNumberOfPrimes.AutoSize = true;
-            this.lblNumberOfPrimes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblNumberOfPrimes.Location = new System.Drawing.Point(313, 197);
-            this.lblNumberOfPrimes.Name = "lblNumberOfPrimes";
-            this.lblNumberOfPrimes.Size = new System.Drawing.Size(187, 25);
-            this.lblNumberOfPrimes.TabIndex = 4137;
-            this.lblNumberOfPrimes.Text = "Há 0 Números Primos";
-            // 
-            // lblTimeElapsed
-            // 
-            this.lblTimeElapsed.AutoSize = true;
-            this.lblTimeElapsed.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblTimeElapsed.Location = new System.Drawing.Point(597, 197);
-            this.lblTimeElapsed.Name = "lblTimeElapsed";
-            this.lblTimeElapsed.Size = new System.Drawing.Size(181, 25);
-            this.lblTimeElapsed.TabIndex = 4137;
-            this.lblTimeElapsed.Text = "Tempo passado: 0Ms";
-            // 
-            // lblPercentage
-            // 
-            this.lblPercentage.AutoSize = true;
-            this.lblPercentage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblPercentage.Location = new System.Drawing.Point(518, 233);
-            this.lblPercentage.Name = "lblPercentage";
-            this.lblPercentage.Size = new System.Drawing.Size(42, 25);
-            this.lblPercentage.TabIndex = 4137;
-            this.lblPercentage.Text = "0 %";
             // 
             // FormPrimeSieve
             // 
@@ -179,23 +196,17 @@ namespace BasicMathBase
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
             this.ClientSize = new System.Drawing.Size(1128, 894);
-            this.Controls.Add(this.lblPercentage);
-            this.Controls.Add(this.lblTimeElapsed);
-            this.Controls.Add(this.lblNumberOfPrimes);
-            this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.richTextBoxLista);
-            this.Controls.Add(this.txtboxUpToPrime);
-            this.Controls.Add(this.btnListPrime);
-            this.Controls.Add(this.lbl_Lista);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormPrimeSieve";
             this.Text = "FormPrimosCrivo";
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -203,14 +214,15 @@ namespace BasicMathBase
 
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.Label lblTimeElapsed;
+        private System.Windows.Forms.Label lblNumberOfPrimes;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPrint;
         private System.Windows.Forms.TextBox txtboxUpToPrime;
         private System.Windows.Forms.Button btnListPrime;
         private System.Windows.Forms.Label lbl_Lista;
         private System.Windows.Forms.RichTextBox richTextBoxLista;
-        private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.Button btnPrint;
-        private System.Windows.Forms.Label lblNumberOfPrimes;
-        private System.Windows.Forms.Label lblTimeElapsed;
-        private System.Windows.Forms.Label lblPercentage;
     }
 }
